b49db6e4929ce012e9d53df9f2ddc36edcb89902
rsfarkas <roxannesfarkas@gmail.com>
2015-11-16 15:46:47 -0500

    test.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+ return num1/num2
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+def argue (name = "Bob")
+  return name
 end
 
-def greeting
-
+def greeting (a, name)
+  a = "Hi"
+return a, name
 end
 
-def return_a_value
-
+def return_a_value (value = "Nice")
+  return value
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(value = "expert")
+  return value
 end
 
-def pizza_party
-
+def pizza_party (food = "cheese")
+  return food
 end
